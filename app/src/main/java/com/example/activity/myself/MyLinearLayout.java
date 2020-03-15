79aee30b77d09f1b3b90b7cafd444890ab1612fe
nlonr <nlonr1989@163.com>
2020-03-15 10:56:57 +0800

    learn fit constraintLayout chain guideline weight

---
+++ app/src/main/java/com/example/activity/myself/MyLinearLayout.java
@@ -0,0 +1,29 @@
+package com.example.activity.myself;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class MyLinearLayout extends LinearLayout {
+
+    public MyLinearLayout(Context context) {
+        super(context);
+    }
+
+    public MyLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int size = Math.min(width, height);
+        setMeasuredDimension(size, size);
+    }
+
+
+}
