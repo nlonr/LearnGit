5290bb51112fb5adfe47d5c8aa5239fc133a4bcd
nlonr <nlonr1989@163.com>
2020-04-09 18:32:04 +0800

    delete unused add build config

---
+++ app/src/main/java/com/example/nlonr/utils/Myself.java
@@ -0,0 +1,29 @@
+package com.example.nlonr.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.content.FileProvider;
+
+
+import java.io.File;
+
+public class Myself {
+
+    private static void startInstallActivity(Context context, String path) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri contentUri = FileProvider.getUriForFile(context,
+                    BuildConfig.APPLICATION_ID + "fileProvider", new File(path));
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setDataAndType(Uri.parse("file://" + path), "application/vnd.android.package-archive");
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+}
